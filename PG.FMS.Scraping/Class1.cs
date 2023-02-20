2f70c88c42b35a8e63e0e2ddaaa93805be3036f7
TigranICT <tigran.ict@gmail.com>
2023-02-20 18:27:34 +0100

    Initial commit

---
+++ PG.FMS.Scraping/Class1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Playwright;
+
+namespace PG.FMS.Scraping;
+
+public class HhLaSailingList
+{
+    public async Task ExecuteAsync()
+    {
+        using var playwright = await Playwright.CreateAsync();
+        await using var browser = await playwright.Chromium.LaunchAsync();
+        var page = await browser.NewPageAsync();
+
+        await page.GotoAsync("https://coast.hhla.de/report?id=Standard-Report-Segelliste");
+    }
+}
